d9409b552a257a8bf0e05c14b14d5ff7bc40d422
NeitanDev <natan.miranda02@gmail.com>
2020-06-18 07:12:14 -0300

    first commit

---
+++ src/receitas/Receitas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package receitas;
+
+/**
+ *
+ * @author natan
+ */
+public class Receitas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
